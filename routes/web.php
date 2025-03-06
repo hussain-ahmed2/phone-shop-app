abaa877e45bf159e2154c689890650d29db5c15f
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-03-06 21:11:55 +0600

    add login and register guest route group

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,17 @@
 <?php
 
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
+// Home
 Route::view('/', 'index');
+
+// User Register and Login
+Route::middleware('guest')->group(function () {
+    Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
+    Route::post('/register', [RegisteredUserController::class, 'store']);
+
+    Route::get('/login', [SessionController::class, 'create'])->name('login');
+    Route::post('/login', [SessionController::class, 'store']);
+});
