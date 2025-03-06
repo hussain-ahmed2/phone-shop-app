d62a47c147cdda95f00908a0c7c447d5bf133d26
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-03-07 03:12:01 +0600

    feat: update navbar with role-based navigation and logout functionality

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,17 +1,29 @@
-<header class="bg-white fixed w-full">
-    <nav class="max-w-7xl mx-auto px-5 shadow-sm border-neutral-300 min-h-14 flex items-center justify-between">
+<header class="bg-white fixed w-full shadow-sm">
+    <nav class="max-w-7xl mx-auto px-5 border-neutral-300 min-h-14 flex items-center justify-between">
         <a href="#" class="logo">Phones</a>
 
         <div class="flex space-x-5">
-            <a class="nav-link" href="#">Home</a>
+            <a class="nav-link" href="/">Home</a>
             <a class="nav-link" href="#">Featured</a>
             <a class="nav-link" href="#">Latest</a>
             <a class="nav-link" href="#">About Us</a>
         </div>
 
-        <div class="flex space-x-5">
+        <div class="flex items-center space-x-5">
             @auth
-                <a class="nav-link" href="#">Dashboard</a>
+                @if (Auth::user()->is_admin)
+                    <a class="btn-sm" href="{{ route('admin.dashboard') }}">Admin Dashboard</a>
+                @else
+                    <a class="btn-sm-bordered" href="/cart">Cart</a>
+                    <a class="btn-sm" href="{{ route('user.dashboard') }}">Dashboard</a>
+                @endif
+                <form method="POST" action="/logout">
+                    @csrf
+                    @method('DELETE')
+                    <button
+                        class="btn-sm-bordered border-rose-500 text-rose-500 hover:text-white hover:bg-rose-500 ring-rose-500/50">Log
+                        Out</button>
+                </form>
             @endauth
 
             @guest
