1c9feed047d5baa137e9706facc42b68c2b6262c
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-03-06 14:39:09 +0600

    create base layout component

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ $pageTitle ?? 'Home' }} | Phones </title>
+
+    {{-- fonts --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,200..800;1,200..800&display=swap"
+        rel="stylesheet">
+
+    <!-- Styles / Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+</head>
+
+<body class="bg-neutral-200 text-neutral-900">
+
+    <x-navbar />
+
+
+    <main class="pt-16 max-w-7xl mx-auto">
+        {{ $slot }}
+    </main>
+
+</body>
+
+</html>
