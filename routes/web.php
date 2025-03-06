8fe4aad83eb022fcba8aae3898efd6ff546d4154
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-03-07 03:12:24 +0600

    feat: add protected routes for user and admin dashboards

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 // Home
@@ -14,4 +16,21 @@ Route::middleware('guest')->group(function () {
 
     Route::get('/login', [SessionController::class, 'create'])->name('login');
     Route::post('/login', [SessionController::class, 'store']);
+});
+
+
+// User Dashboard (Only Authenticated Users)
+Route::middleware(['auth'])->group(function () {
+    Route::delete('/logout', [SessionController::class, 'destroy'])->name('logout');
+    Route::get('/dashboard', [UserController::class, 'index'])->name('user.dashboard');
+});
+
+// Admin Dashboard (Only Admins)
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::redirect('/admin', '/admin/dashboard');
+    Route::get('/admin/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
+    Route::get('/admin/phones', [AdminController::class, 'phones'])->name('admin.phones.index');
+    Route::get('/admin/orders', [AdminController::class, 'orders'])->name('admin.orders.index');
+    Route::get('/admin/users', [AdminController::class, 'users'])->name('admin.users.index');
+    Route::get('/admin/settings', [AdminController::class, 'settings'])->name('admin.settings.index');
 });
