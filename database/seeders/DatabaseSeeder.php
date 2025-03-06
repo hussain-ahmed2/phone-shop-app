f2c6959f0becc72cbc401c46479479e0203bc769
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-03-07 03:11:16 +0600

    feat: update database seeder with admin user and enable user factory seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'firstname' => 'Hussain',
+            'lastname' => 'Ahmed',
+            'email' => 'hussain@example.com',
+            'password' => 'password',
+            'is_admin' => true
         ]);
     }
 }
