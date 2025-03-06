b0d01ec5a88d8be09be399ba58d662cf08b96394
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-03-07 03:10:33 +0600

    feat: implement user authentication with login and logout functionality

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -3,11 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Email;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Validation\ValidationException;
 
 class SessionController extends Controller
 {
     public function create() 
     {
         return view('auth.login');
+    }
+
+    public function store(Request $request) 
+    {
+        $user = $request->validate([
+            'email' => ['required', Email::default()],
+            'password' => ['required', Password::min(6)]
+        ]);
+
+        if (! Auth::attempt($user)) {
+            throw ValidationException::withMessages([
+                'email' => 'Sorry, those credentials are incorrect.'
+            ]);
+        }
+
+        $request->session()->regenerate();
+
+        return redirect('/');
+    }
+
+    public function destroy(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/');
     }
 }
