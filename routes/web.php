9456e1680e574ce34c00ac48a55c3c17df4f86ad
Hussain Ahmed <hussainahmed.vu@gmail.com>
2025-03-08 00:30:18 +0600

    Update routes for phone management in admin section

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,11 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
 use App\Http\Controllers\UserController;
+use App\Models\Phone;
 use Illuminate\Support\Facades\Route;
 
 // Home
-Route::view('/', 'index');
+Route::view('/', 'index', ['phones' => Phone::latest()->take(3)->get()]);
 
 // User Register and Login
 Route::middleware('guest')->group(function () {
@@ -29,7 +30,14 @@ Route::middleware(['auth'])->group(function () {
 Route::middleware(['auth', 'admin'])->group(function () {
     Route::redirect('/admin', '/admin/dashboard');
     Route::get('/admin/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
+
     Route::get('/admin/phones', [AdminController::class, 'phones'])->name('admin.phones.index');
+    Route::get('/admin/phones/create', [AdminController::class, 'create_phone'])->name('admin.phones.create');
+    Route::post('/admin/phones', [AdminController::class, 'store_phone']);
+    Route::get('/admin/phones/{id}/edit', [AdminController::class, 'edit_phone'])->name('admin.phones.edit');
+    Route::put('/admin/phones/{id}', [AdminController::class, 'update_phone']);
+    Route::delete('/admin/phones/{id}', [AdminController::class, 'delete_phone']);
+
     Route::get('/admin/orders', [AdminController::class, 'orders'])->name('admin.orders.index');
     Route::get('/admin/users', [AdminController::class, 'users'])->name('admin.users.index');
     Route::get('/admin/settings', [AdminController::class, 'settings'])->name('admin.settings.index');
